c8bc5dd897eb4d5dda8105b70cb89614e568bae9
Matt Brown <matthewbrown0798@gmail.com>
2020-12-17 11:07:03 -0500

    modularized project list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,29 +1,13 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title"></span>
-          <p>Posted by Matt</p>
-          <div className="grey-text">2020</div>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title"></span>
-          <p>Posted by Matt</p>
-          <div className="grey-text">2020</div>
-        </div>
-      </div>
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title"></span>
-          <p>Posted by Matt</p>
-          <div className="grey-text">2020</div>
-        </div>
-      </div>
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
+      <ProjectSummary />
     </div>
   );
 };
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProjectSummary = () => {
+  return (
+    <div className="card z-depth-0 project-summary">
+      <div className="card-content grey-text text-darken-3">
+        <span className="card-title"></span>
+        <p>Posted by Matt</p>
+        <div className="grey-text">2020</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectSummary;
